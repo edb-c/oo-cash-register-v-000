44fd9ebd094e574bc188af7e4d184fb8066fe7b8
Ethel C <cofresi.ethel@gmail.com>
2018-07-06 03:27:38 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -84,4 +84,4 @@ class CashRegister
   #cash_register_with_discount.items 
   #cash_register_with_discount.void_last_transaction
 cash_register = CashRegister.new(10)
-puts cash_register.price
+puts cash_register.discount
